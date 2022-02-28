15f973801a5eb9dffa7416a3a1f5aea720e39501
adarshgupta0040 <73924145+adarshgupta0040@users.noreply.github.com>
2022-02-28 13:48:30 +0530

    Add files via upload

---
+++ miniproject_app.py
@@ -0,0 +1,42 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__,template_folder = "psit_project")
+
+model=pickle.load(open('save.pkl','rb'))
+
+@app.route('/')
+def hello_world():
+    return render_template("customer.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features[1:])]
+    print(int_features)
+    print(final)
+    
+    prediction=model.predict(final)
+
+    #output='{0:.{1}f}'.format(prediction[0][1], 2)
+    if prediction[0] == 1:
+        return  render_template('customer.html',pred="earning high but spending less")
+    elif prediction[0] == 2:
+        return render_template('customer.html',pred="average in terms of earning and spending ")
+    elif prediction[0] == 3:
+        return render_template('customer.html',pred="earning high and also spending high")
+    elif prediction[0] == 4:
+        return render_template('customer.html',pred="earning less but spending more")
+    else:
+        return render_template('customer.html',pred="Earning less , spending less")
+    #return render_template('customer.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(prediction))
+
+    #if output>str(0.5):
+        #return render_template('customer.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    #else:
+        #return render_template('customer.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+
+if __name__ == '__main__':
+    app.run(debug=True)
